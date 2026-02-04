5a49a5e7598dbd118f9f9b42769e13a318034da0
anhuynh03 <au786725@uni.au.dk>
2026-02-04 21:49:37 +0100

    kommentarer

--- ShopAbs/ShopC.h
+++ ShopAbs/ShopC.h
@@ -1,21 +1,23 @@
 #include <stdbool.h>
 
+
+// functions for operating a ShoeShop
 void add_items(struct ShoeShop, int, int);
 int in_stock(struct ShowShop, int);
 int count_items(struct ShoeShop);
 bool clear(struct ShoeShop, int);
 
-struct Shop {
+struct Shop {   // struct for a generic shop
 
-    void (* add_items_func)(struct ShoeShop, int, int);
-    int (* in_stock_func)(struct ShoeShop, int);
-    int (* count_items_func)(struct ShoeShop);
-    bool (* clear_func)(struct ShoeShop, int);
+    void (* add_items_func)(struct ShoeShop, int, int); // adds a quantity of an item
+    int (* in_stock_func)(struct ShoeShop, int);        // checks stock on an item
+    int (* count_items_func)(struct ShoeShop);          // counts total items
+    bool (* clear_func)(struct ShoeShop, int);          // removes an item
 };
 
-struct ShoeShop {
-    int * shelf;
-    int size;
+struct ShoeShop {   // struct for a ShoeShop, this almost looks identical to the shop struct
+    int * shelf;    // array storing number of items
+    int size;       // number of slots in the shelf
 
     void (* add_items_func)(struct ShoeShop, int, int);
     int (* in_stock_func)(struct ShoeShop, int);
--- ShopOrg/ShoeShopStruct.c
+++ ShopOrg/ShoeShopStruct.c
@@ -1,13 +1,16 @@
 #include "ShoeShopStructC.h"
 
+// adds the number of items to a specific row in the shelf
 void add_items(struct ShoeShop * s, int row, int num){
     s->shelf[row] += num;
 }
  
+// checks if the stock in the row of the shelf is 0 or not
 int in_stock(struct ShoeShop * s, int row){
     return s->shelf[row] > 0;
 }
 
+// counts the total number of items for all shelves
 int count_items(struct ShoeShop * s){
     int tot = 0;
     for (int k = 0; k < s->size; k++) {
@@ -16,25 +19,28 @@ int count_items(struct ShoeShop * s){
     return tot;
 }
 
+// clear a specific shelf row. returns true if the shelf contained any items, otherwise false
 bool clear(struct ShoeShop * s, int row){
     bool rem = s->shelf[row] > 0;
     s->shelf[row] = 0;
     return rem;
 }
 
+// deletes a ShoeShop
 void ShoeShopDestructor(struct ShoeShop * s) {
     free(s);
    
 }
 
+// constructs a ShoeShop
 struct ShoeShop ShoeShopConstructor(int size) { 
     struct ShoeShop * shoeshop = malloc(sizeof(struct ShoeShop));
     
     shoeshop->shelf = malloc(sizeof(int[100])); // vi må ikke alokere dynamisk så tallet bliver sat til 100
-    shoeshop->size = size;
+    shoeshop->size = size;                      // number of slots in the shelf
 
-    shoeshop->add_items_func = &add_items;
-    shoeshop->in_stock_func = &in_stock;
-    shoeshop->count_items_func = &count_items;
-    shoeshop->clear_func = &clear;
+    shoeshop->add_items_func = &add_items;      // adds a quantity of an item, calls the add_item function
+    shoeshop->in_stock_func = &in_stock;        // checks stock on an item, calls the in_stock function
+    shoeshop->count_items_func = &count_items;  // counts total items, calls the count_items function
+    shoeshop->clear_func = &clear;              // removes an item, calls the clear function
 }
